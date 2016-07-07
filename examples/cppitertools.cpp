0d0cd35d02f7983fa098a592407ec8fd66601934
Alan <alan.patterson5@gmail.com>
2016-07-07 18:50:21 +0100

    initial prep to push to github.

--- examples/cppitertools.cpp
+++ examples/cppitertools.cpp
@@ -3,12 +3,6 @@
 #include "cppitertools/enumerate.hpp"
 #include "cppitertools/range.hpp"
 
-template <typename T, typename RangeT,
-          template <typename> class Container = std::vector>
-Container<T> make(RangeT range) {
-  return Container<T>(std::begin(range), std::end(range));
-}
-
 int main(int argc, char* argv[]) {
   std::vector<int> v = {0, 1, 2, 3};
   for (auto&& n : iter::enumerate(v)) {
@@ -18,9 +12,5 @@ int main(int argc, char* argv[]) {
   auto range = iter::range(10);
   std::vector<int> r(std::begin(range), std::end(range));
   std::cout << r.size() << std::endl;
-
-  auto range2 = iter::range(5);
-  auto c = make<>(range2);
-  std::cout << c.size() << std::endl;
   return 0;
 }
